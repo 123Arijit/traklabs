b5883c4b3f477d7df0eefdc86d254c8a671f05e2
123Arijit <51528279+123Arijit@users.noreply.github.com>
2020-12-31 12:11:05 +0530

    fix: app cors

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(cors)
+app.use(<any>cors())
 
-app.use("/r", RecordRouter)
+app.use("/", RecordRouter)
 
 export default app
